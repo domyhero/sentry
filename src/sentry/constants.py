bea8d3ad293ac5eec2dc3709f675f137e60f26bb
Eric Feng <erichfeng@gmail.com>
2017-07-19 16:40:19 -0700

    adding onboarding to the reserved slugs

--- src/sentry/constants.py
+++ src/sentry/constants.py
@@ -84,7 +84,7 @@ RESERVED_ORGANIZATION_SLUGS = frozenset((
     'subscribe', 'enterprise', 'about', 'jobs', 'thanks', 'guide',
     'privacy', 'security', 'terms', 'from', 'sponsorship', 'for',
     'at', 'platforms', 'branding', 'vs', 'answers', '_admin',
-    'support', 'contact',
+    'support', 'contact', 'onboarding',
 ))
 
 LOG_LEVELS = {
